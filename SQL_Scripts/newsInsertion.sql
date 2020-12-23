b4e6ae1bc139d09b94641412727f0839e2c3e1c4
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2020-12-23 22:55:41 +0100

    organizacao

--- SQL_Scripts/newsInsertion.sql
+++ SQL_Scripts/newsInsertion.sql
@@ -1,8 +1,2 @@
-drop table news;
-create table news
-(
-	date varchar(256) not null,
-	info varchar(256) not null
-);
 insert into news(date, info) values('26/12/20', 'Lançamento da Unicloud');
 insert into news(date, info) values('24/12/20', 'Boas Festas!');
