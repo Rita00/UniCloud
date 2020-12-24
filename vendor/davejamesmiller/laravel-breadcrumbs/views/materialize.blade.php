ad4994db6073e61f44182c802e3625465725fcf9
FMiranda97 <brugesmiranda@gmail.com>
2020-12-24 02:39:55 +0000

    production env

--- vendor/davejamesmiller/laravel-breadcrumbs/views/materialize.blade.php
+++ vendor/davejamesmiller/laravel-breadcrumbs/views/materialize.blade.php
@@ -6,7 +6,7 @@
                 @foreach ($breadcrumbs as $breadcrumb)
 
                     @if ($breadcrumb->url && !$loop->last)
-                        <a href="{{ $breadcrumb->url }}" class="breadcrumb">{{ $breadcrumb->title }}</a>
+                        <a href="{{ $breadcrumb->url }}" class="breadcrumb">{{ $breadcrumb->title }}</a> >
                     @else
                         <span class="breadcrumb">{{ $breadcrumb->title }}</span>
                     @endif
